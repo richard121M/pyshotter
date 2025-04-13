1fceb7a8a8c3d1b79bc52644743f6f3962271a45
richard121M <richardtavares520@gmail.com>
2025-04-13 11:31:25 -0300

    shotter v0.03

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 from pygame import *
 from math import *
+
 init()
 
 tile_map = [
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-            [1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,1,1,1,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0],
+            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
             ]
-
-
 
 list_spr = []
 list_colider = []
@@ -42,6 +41,10 @@ def intesec_rects(rectA,rectB,dire):
     if mirol_.colliderect(rectB):
         return True
     return False
+
+def lerp(x,y,peso):
+    dists = y-x
+    return x + dists*peso
 
 def direct(Vec01: Vector2,Vec02: Vector2):
     dire = Vector2(0,0)
@@ -72,10 +75,7 @@ class TEMPORIZADOR():
 
 class Bullet():
     def __init__(self,x,y,dire_x,dire_y):
-        self.img = Surface((8,8))
-        spr = image.load("bullet_spr.png")
-        self.img.blit(spr,(0,0))
-
+        self.img = image.load("bullet_spr.png")
         self.rect = self.img.get_rect()
         self.pos = math.Vector2(Vector2(x,y))
         self.rect.center = self.pos
@@ -85,29 +85,42 @@ class Bullet():
         self.pos.y += self.dire.y*2.5
         self.pos.x += self.dire.x*2.5
         self.rect.center = self.pos
-
+        if intsec_listRect(self.rect,list_colider,Vector2(0,0)):
+            list_spr.remove(self)
+            list_bullet.remove(self)
     def draw(self,surfac):
         surfac.blit(self.img,self.rect)
 
 class PLAYER():
     def __init__(self,x,y):
-        self.img = Surface((16,16))
-        self.img.fill((200,0,0))
+        self.imgs = [image.load("robo_and2.png"),image.load("robo_and.png"),image.load("robo_and03.png")]
+        self.img = self.imgs[0]
+        self.img_index = 0
+        
         self.rect = self.img.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.time_shotter = TEMPORIZADOR(15)
+        self.ref_ani = 1
     def updade(self):
+        self.img_index += 0.2*self.ref_ani
+        if int(self.img_index) >= 3 or self.img_index <= 0:
+            self.ref_ani *= -1
+            self.img_index += 0.2*self.ref_ani
+
+        
         keys = key.get_pressed()
         dire = Vector2(keys[K_d]-keys[K_a],keys[K_s]-keys[K_w])
+        self.img = transform.flip(self.imgs[int(self.img_index)],keys[K_a],False)
 
-        if intsec_listRect(self.rect,list_colider,Vector2(keys[K_d]-keys[K_a],0)):
+        if intsec_listRect(self.rect,list_colider,Vector2(dire.x*2,0)):
             dire.x = 0
-        if intsec_listRect(self.rect,list_colider,Vector2(0,keys[K_s]-keys[K_w])):
+        if intsec_listRect(self.rect,list_colider,Vector2(0,dire.y*2)):
             dire.y = 0
         
         Mous = mouse.get_pressed()
         self.time_shotter.rodar()
+
         if Mous[0] == True and self.time_shotter.is_finish():
             mouse_pos = list(mouse.get_pos())
             mouse_pos[0] += -384/2 + self.rect.x
@@ -120,8 +133,8 @@ class PLAYER():
             list_spr.append(bullet)
             self.time_shotter.reniciar()
 
-        self.rect.x += dire.x
-        self.rect.y += dire.y
+        self.rect.x += dire.x*2
+        self.rect.y += dire.y*2
     def draw(self,surfac):
         surfac.blit(self.img,self.rect)
 
@@ -161,15 +174,17 @@ def inter_tile(TileMap : list):
     for list_x in tilemap:
         pos_x = 0
         for i in list_x:
+            if i == 1:
+                tilemap[pos_y][pos_x] = 0
             if Tx == -1:
                 if i == 1:
                     Ty = pos_y
                     Tx = pos_x
             if Tx != -1:
                 if i == 0:
-                    Bx = pos_x-1 
+                    Bx = pos_x -1
             if Tx !=-1 and Bx != -1:
-                ye = Ty
+                ye = Ty+1
                 while(By ==-1):
                     for a in range(Tx,Bx+1):
                         if tilemap[ye][a] != 1:
@@ -199,8 +214,10 @@ class CAMERA():
         self.tela_img = display.get_surface()
         self.pos = Vector2(0,0)
     def draw_screen(self,surfac):
-        self.pos = Vector2(player.rect.x+8-384/2,player.rect.y+8-216/2)
-
+        x_ = lerp(self.pos.x,(player.rect.x+8-384/2),0.06)
+        y_ = lerp(self.pos.y,(player.rect.y+8-216/2),0.06)
+        self.pos = Vector2(x_,y_)
+        
         for spr in list_spr:
             # rec = spr.rect
             pos = [spr.rect.x,spr.rect.y]
@@ -224,7 +241,7 @@ while LOOP:
         i.updade()
 
     screen.fill((0, 0, 0))
-    canva.fill((20,0,20))
+    canva.fill((0,50,100))
     player.updade()
     cam.draw_screen(canva)
     screen.blit(canva,(0,0))
